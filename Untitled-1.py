c744049eaf6670344197e257d47b406ca4e89fb9
eve55555 <eve.ggu@gmail.com>
2025-03-17 11:48:42 +0100

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,34 @@
+#Interface graphique fonctions et variables pas complètes
+#finir de def les variables (tk, creer_paquet)
+class BlackjackApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Jeu de Blackjack")
+
+        self.paquet = creer_paquet()
+        self.main_joueur = [self.paquet.pop(), self.paquet.pop()] #fois 2
+        self.main_croupier = [self.paquet.pop(), self.paquet.pop()]
+
+        self.canvas = tk.Canvas(root, width=600, height=400, background="green")
+        self.canvas.pack()
+
+        self.afficher_cartes()
+
+        self.bouton_tirer = tk.Button(root, text="Tirer une carte", command=self.tirer_carte)
+        self.bouton_tirer.pack(side=tk.LEFT, padx=20)
+
+        self.bouton_rester = tk.Button(root, text="Rester", command=self.rester)
+        self.bouton_rester.pack(side=tk.LEFT, padx=20)
+
+        self.message = tk.Label(root, text="Tour du joueur", bg="green", fg="white")
+        self.message.pack()
+
+  
+
+
+
+
+
+
+
+
