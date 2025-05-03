0c7ebd7075439aa7f93cb3f07b83121e2122a54b
CharalamposV <charavekas@gmail.com>
2025-05-03 13:39:55 +0200

    translate

--- interface  lancement jeu.py
+++ interface  lancement jeu.py
@@ -1,6 +1,7 @@
 from random import *
 import tkinter as tk
 import random
+
 def cartes_deck():
     couleur = ['carreau','coeur','pique','trefle']
     valeur = ['as','2','3','4','5','6','7','8','9','10','valet','dame','roi']
